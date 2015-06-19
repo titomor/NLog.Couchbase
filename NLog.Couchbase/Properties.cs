624022caca65a408add7ae711f8ab24b7afe7b50
titomor <titomor@gmail.com>
2015-06-19 17:08:53 +0100

    Added support for mappings

---
+++ NLog.Couchbase/Properties.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace NLog.Couchbase
+{
+    public class Properties
+    {
+        public Properties()
+        {
+            Excludes = new List<Exclude>();
+            Includes = new List<Include>();
+        }
+        [NLog.Config.ArrayParameter(typeof(NLog.Couchbase.Exclude), "exclude")]
+        public IList<Exclude> Excludes { get; set; }
+
+        [NLog.Config.ArrayParameter(typeof(NLog.Couchbase.Include), "include")]
+        public IList<Include> Includes { get; set; }
+    }
+
+    
+    public class Exclude
+    {
+        public ObjectType Path { get; set; }
+        public string Name { get; set; }
+    }
+    
+    public class Include
+    {
+        public ObjectType PropertyType { get;set; }
+        public string Name { get; set; }
+        public string MapTo { get; set; }
+    }
+
+    public enum ObjectType
+    {
+        LogEventInfo = 0,
+        Properties = 1,
+        Parameters = 2 //NOT IMPLEMENTED
+    }
+
+}
